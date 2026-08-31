76d058c6a96b8bb4abb388e62e3cad0f73561618
𝘼𝙫𝙞𝙣𝙖𝙨𝙝 <avinashk08408@gmail.com>
2026-08-31 23:21:59 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Events from "./components/Events";
+import Footer from "./components/Footer";
+import EventPage from "./pages/EventPage";
+
+import "./App.css";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+
+      <main>
+        <Hero />
+        <About />
+        <Events />
+      </main>
+
+      <Footer />
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/events/:eventId" element={<EventPage />} />
+    </Routes>
+  );
+}
+
+export default App;
