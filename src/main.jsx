8c6ec5af38194a70086d01692baa201d9a3b5af7
𝘼𝙫𝙞𝙣𝙖𝙨𝙝 <avinashk08408@gmail.com>
2026-09-01 11:24:42 +0530

    Replace BrowserRouter with HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
   <React.StrictMode>
-    <BrowserRouter basename="/sympo">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
