77bf9d0a912fcf2a485d39d3ba12b0b99aed6a4e
𝘼𝙫𝙞𝙣𝙖𝙨𝙝 <avinashk08408@gmail.com>
2026-09-01 22:48:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,6 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Events from "./components/Events";
 import Footer from "./components/Footer";
-
-import EventCoordinators from "./components/EventCoordinators";
-import ZyverseTeam from "./components/ZyverseTeam";
-
 import EventPage from "./pages/EventPage";
 
 import "./App.css";
@@ -19,23 +15,11 @@ function Home() {
       <Navbar />
 
       <main>
-        {/* HERO */}
         <Hero />
-
-        {/* ABOUT */}
         <About />
-
-        {/* FIVE EVENTS */}
         <Events />
-
-        {/* EVENT COORDINATORS */}
-        <EventCoordinators />
-
-        {/* ZYVERSE TEAM */}
-        <ZyverseTeam />
       </main>
 
-      {/* FOOTER */}
       <Footer />
     </>
   );
@@ -44,10 +28,8 @@ function Home() {
 function App() {
   return (
     <Routes>
-      {/* MAIN WEBSITE */}
       <Route path="/" element={<Home />} />
 
-      {/* INDIVIDUAL EVENT PAGE */}
       <Route
         path="/events/:eventId"
         element={<EventPage />}
