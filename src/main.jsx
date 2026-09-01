9a1338af38d675dbdff979cb7e664b1542698099
𝘼𝙫𝙞𝙣𝙖𝙨𝙝 <avinashk08408@gmail.com>
2026-09-01 17:09:49 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/sympo">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
